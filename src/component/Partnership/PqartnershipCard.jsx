225873baed560db4b20fe13485a7b00f9ba194e6
Sujokum <suneelahmed413@gmail.com>
2023-07-25 12:58:55 +0500

    addChanges

--- src/component/Partnership/PqartnershipCard.jsx
+++ src/component/Partnership/PqartnershipCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PqartnershipCard = ({para  , title}) => {
   return (
   
-    <div className='w-[426px] py-[50px] rounded-[20px] gap-y-[30px] bg-white shadow flex items-center justify-start flex-col ' >
+    <div className='xl:w-[350px] lg:w-[300px] max-lg:w-[426px] 2xl:w-[426px] py-[50px] rounded-[20px] gap-y-[30px] bg-white shadow flex items-center justify-start flex-col ' >
         {title}
         {para}
     </div>
